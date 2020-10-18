b9e1f24432cdd505da6bbb14c60ca8c267c28af8
diegoperez03 <diego03@ucsb.edu>
2020-10-17 18:43:49 -0700

    Added scrolling to friendsview and up/down button functionality

--- friendslist/src/Components/FriendsItem.tsx
+++ friendslist/src/Components/FriendsItem.tsx
@@ -23,18 +23,30 @@ function FriendsItem(props: FriendsItemProps) {
         red += time *8 
     } else {
         red = 255
-        green -= (time - 31) * 8
+        green -= (time - 28) * 8
     }
     if(green < 0){
         green = 0
     }
-    return rgbToHex(red,green,0)
+    return rgbToHex(red,green,50)
   }
 
   const timeFormatString = () => {
       //todo
+        if(time < 1) {
+            return "today"
+        }
+       else if(time < 7) {
+          return time.toString(10) + (time == 1 ? " day" : " days")
+      } else if (time < 29) {
+          const weeks = Math.floor(time / 7)
+          return weeks.toString(10) + (weeks == 1 ? " week" : " weeks")
+      } else {
+          const month = Math.floor(time / 31)
+          return month.toString(10) + (month == 1 ? " month" : " months")
+      }
   }
- 
+
   return (
     <div className="FriendsItem">
         <Grid container>
@@ -55,12 +67,15 @@ function FriendsItem(props: FriendsItemProps) {
                         <Typography> {name} </Typography>
                         <Typography> {relation}</Typography>
                     </Grid>
-                    <Grid item xs={2}>
-                        <Error className="Fav" />
+                    <Grid item xs={2} 
+                    >
+                        <Error className="Fav"
+                style={{background:colorFunc()}}
+                                                />
                     </Grid>
                     <Grid item xs={4}>
                         <Typography> Last Contacted: </Typography>
-                        <Typography> {time} Months </Typography>
+                        <Typography> {timeFormatString()} </Typography>
                     </Grid>
                 </Grid>
             </Grid>
--- friendslist/src/Components/FriendsView.tsx
+++ friendslist/src/Components/FriendsView.tsx
@@ -1,30 +1,86 @@
-import { Button, Grid } from '@material-ui/core';
+import { Button, Fab, Grid } from '@material-ui/core';
 import React from 'react';
 import FriendsItem from './FriendsItem';
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons'
 
 function FriendsView() {
-    const friends = ["Diego", "Gus", "Pedro", "Steven","RJ"]
+    const friends = ["Diego", "Gus", "Pedro", "Steven","RJ","A","b","c"]
+    const [topScroll, setTopScroll] = React.useState<boolean>(false)
+    const [bottomScroll, setBottomScroll] = React.useState<boolean>(false)
+    const FriendsViewEl = document.getElementsByClassName("FriendsView")
+    const NofFriend = friends.length
+    
+
+    React.useEffect(() => {
+        let element = (FriendsViewEl[0] as Element)
+
+        if(NofFriend > 5 ) {
+            setBottomScroll(true)
+        }
+        setInterval(() => {
+            if(element.scrollTop > 0){
+                setTopScroll(true)
+            } else {setTopScroll(false)}
+
+            if(element.scrollHeight - element.scrollTop - element.clientHeight < 5){
+                setBottomScroll(false)
+            } else { setBottomScroll(true)}
+
+        },10)
+    },[])
+
+    const scrollBottom = () => {
+        let element = (FriendsViewEl[0] as Element)
+        element.scrollTop = element.clientHeight
+    }
+
+    const scrollTop = () => {
+        let element = (FriendsViewEl[0] as Element)
+        element.scrollTop = 0
+    }
   return (
-    <div className="FriendsView">
+    <div>
         <Grid container>
+            <Grid item xs={5} />
+                <Grid item xs={2} >
+                    {topScroll && 
+                        <Button onClick={() => scrollTop()}>
+                            <Fab color="primary" >
+                                <ArrowUpward />
+                            </Fab>
+                        </Button>
+                    }
+                </Grid>
+            <Grid item xs={5}/>
+        </Grid>
+        <Grid container  className="FriendsView">
             {
                 friends.map((friend:string) => {
                     return (
                     <Grid item xs={12}>
+                        <Button>
                         <FriendsItem 
                             name={friend}
                             relation="whatever"
-                            time={90}
+                            time={0}
                             picLink="https://ichef.bbci.co.uk/news/800/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg"
                             favorited={true}
                         />
+                        </Button>
+                        
                     </Grid>
                     )
                 })
             }
-            <Grid item xs={5} />
+        </Grid>
+        <Grid container>
+        <Grid item xs={5} />
             <Grid item xs={2} >
-                <Button variant="contained"/>
+                {bottomScroll &&
+                    <Fab color="primary" onClick={() => scrollBottom()} >
+                    <ArrowDownward />
+                </Fab>
+                }
             </Grid>
             <Grid item xs={5}/>
         </Grid>
